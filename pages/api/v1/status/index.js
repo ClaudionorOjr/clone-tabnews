582cd2470f26c4ed7ca8e19823508de015b85373
ClaudionorOjr <claudionorojr@hotmail.com>
2025-11-06 19:01:42 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,6 +2,7 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
@@ -19,7 +20,6 @@ async function status(request, response) {
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
-  // console.log();
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
