15c4111b42b824413ebe096038756490adc8e666
ClaudionorOjr <claudionorojr@hotmail.com>
2025-11-14 18:41:54 +0000

    fix `/migrations` endpoint connection bug

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ git log --stat <!-- Indica os arquivos modificaos no commit -->
 git log --oneline <!-- Retorna os commits de forma compactada -->  
 git commit --amend <!-- "Emenda" tudo que estiver em staged ao commit anterior. Ótimo para corrigir algo que esqueceu de adicionar ao commit, sem precisar criar um novo para uma pequena alteração. -->  
 git restore . <!-- Restaura todos os arquivos para a versão que estavam no último commit -->
+| <!-- Operador Pipe. Pega a saída de um comando (output) e injeta na entrada de outro comando (input). Exemplo de uso: curl https://clone-tabnews-git-fix-migrations-461f1e-claudionorojrs-projects.vercel.app/api/v1/status | python3 -m json.tool -->
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,35 +3,47 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const defaultMigrationOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-
-    return response.status(200).json(pendingMigrations);
+  const allowerdMethods = ["GET", "POST"];
+  if (!allowerdMethods.includes(request.method)) {
+    return response
+      .status(405)
+      .json({ error: `Method ${request.method} not allowed` });
   }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      const statusCode = migratedMigrations.length > 0 ? 201 : 200;
+
+      return response.status(statusCode).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await dbClient.end();
-
-    const statusCode = migratedMigrations.length > 0 ? 201 : 200;
-
-    return response.status(statusCode).json(migratedMigrations);
   }
-
-  return response.status(405).end(); // Method not allowed
 }
