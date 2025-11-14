c82332f434356457d9905afae6c46d133943a1b5
ClaudionorOjr <claudionorojr@hotmail.com>
2025-11-14 17:32:19 +0000

    refectored status code in live run

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -28,11 +28,9 @@ export default async function migrations(request, response) {
     });
     await dbClient.end();
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
+    const statusCode = migratedMigrations.length > 0 ? 201 : 200;
 
-    return response.status(200).json(migratedMigrations);
+    return response.status(statusCode).json(migratedMigrations);
   }
 
   return response.status(405).end(); // Method not allowed
